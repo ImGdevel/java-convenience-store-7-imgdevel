a01fe2191db6d84abf07e860c4a781e372d73c4c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-09 18:41:34 +0900

    test(ProductTest): Product 클래스 기능 테스트

---
+++ src/test/java/store/domain/ProductTest.java
@@ -0,0 +1,41 @@
+package store.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    private Product createProduct() {
+        return new Product("콜라", 1000, 10, "탄산2+1");
+    }
+
+    @Test
+    void 프로모션이_있을_경우_출력() {
+        Product product = createProduct();
+
+        assertEquals("콜라 1,000원 10개 탄산2+1", product.toString());
+    }
+
+    @Test
+    void 프로모션이_없을_경우_출력() {
+        Product product = new Product("사이다", 1000, 8, null);
+
+        assertEquals("사이다 1,000원 8개", product.toString());
+    }
+
+    @Test
+    void 재고가_차감되는지_확인() {
+        Product product = createProduct();
+
+        product.reduceStock(3);
+        assertEquals(7, product.getStock());
+    }
+
+    @Test
+    void 재고가_0이_될_때_차감후_재고확인() {
+        Product product = createProduct();
+
+        product.reduceStock(10);
+        assertEquals(0, product.getStock());
+    }
+}

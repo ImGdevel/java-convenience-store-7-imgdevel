0f876b03beb5ca5ebf25708c2d29896dc7d8a918
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-09 17:47:03 +0900

    feat(DataLoader): 데이터를 불러오는 DataLoader
    리소스폴더에 있는 상품과 프로모션 정보를 불러온다.
    각 정보는 List에 저장하여 반환한다.
    
    삼품과 프로모션 Join 관계를 가지고 있다.
    해당 구현시 데이터를 어떻게 할것인가?

---
+++ src/main/java/store/infra/DataLoader.java
@@ -0,0 +1,57 @@
+package store.infra;
+
+import store.domain.Product;
+import store.domain.Promotion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataLoader {
+
+    private final String PRODUCT_PATH = "src/main/resources/products.md";
+    private final String PROMOTION_PATH = "src/main/resources/promotions.md";
+
+
+    public List<Product> loadProduct() throws IOException {
+        List<Product> products = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Path.of(PRODUCT_PATH));
+
+        for (String line : lines) {
+            if (line.isBlank() || line.startsWith("name")) continue;
+
+            String[] fields = line.split(",");
+            String name = fields[0];
+            int price = Integer.parseInt(fields[1]);
+            int quantity = Integer.parseInt(fields[2]);
+            Promotion promotion = null; // todo : 프로모션은 어떻게 처리? Enum?
+
+            products.add(new Product(name, price, quantity, promotion));
+        }
+        return products;
+
+    }
+
+    public List<Promotion> loadPromotions() throws IOException {
+        List<Promotion> promotions = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Path.of(PROMOTION_PATH));
+
+        for (String line : lines) {
+            if (line.isBlank() || line.startsWith("name")) continue;
+
+            String[] fields = line.split(",");
+            String name = fields[0];
+            int buyQuantity = Integer.parseInt(fields[1]);
+            int getQuantity = Integer.parseInt(fields[2]);
+            LocalDate startDate = LocalDate.parse(fields[3]);
+            LocalDate endDate = LocalDate.parse(fields[4]);
+
+            promotions.add(new Promotion(name, buyQuantity, getQuantity, startDate, endDate));
+        }
+        return promotions;
+    }
+
+}

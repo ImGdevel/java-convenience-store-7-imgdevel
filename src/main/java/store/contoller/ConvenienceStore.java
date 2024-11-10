67d551a77af2ed9a7403b48654ad6cb57bfca2b7
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-11 03:11:49 +0900

    feat(ConvenienceStore): Controller 작성
    매인 로직을 담당하는 Controller 작성, 메서드 분리 필요

--- src/main/java/store/contoller/ConvenienceStore.java
+++
@@ -1,4 +0,0 @@
-package store.contoller;
-
-public class ConvenienceStore {
-}
---
+++ src/main/java/store/controller/ConvenienceStore.java
@@ -0,0 +1,109 @@
+package store.controller;
+
+import store.Service.InventoryService;
+import store.Service.PromotionService;
+import store.domain.Order;
+import store.domain.ProductOrder;
+import store.infra.ProductDataLoader;
+import store.infra.PromotionDataLoader;
+import store.repository.InventoryManager;
+import store.repository.PromotionManager;
+import store.view.InputView;
+import store.view.OutputView;
+
+import java.util.List;
+
+public class ConvenienceStore {
+
+    final String productsDataPath = "src/main/resources/products.md";
+    final String promotionDataPath = "src/main/resources/promotions.md";
+
+    InputView inputView;
+    OutputView outputView;
+
+    ProductDataLoader productDataLoader;
+    PromotionDataLoader promotionDataLoader;
+
+    InventoryManager inventoryManager;
+    PromotionManager promotionManager;
+
+    PromotionService promotionService;
+    InventoryService inventoryService;
+
+    public ConvenienceStore(){
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+
+        this.productDataLoader = new ProductDataLoader(productsDataPath);
+        this.promotionDataLoader = new PromotionDataLoader(promotionDataPath);
+
+        this.inventoryManager = new InventoryManager(productDataLoader);
+        this.promotionManager = new PromotionManager(promotionDataLoader);
+
+        this.inventoryService = new InventoryService(inventoryManager);
+        this.promotionService = new PromotionService(inventoryManager, promotionManager);
+    }
+
+    public void start(){
+        while (true){
+
+            outputView.displayWelcomeMessage();
+            outputView.displayProductList(inventoryService.getProducts());
+
+            List<ProductOrder> orders = InputPurchase();
+            List<ProductOrder> promotionProduct = promotionService.getPromotionProduct(orders);
+
+            Order order = new Order(orders, promotionProduct); // 주문서 생성
+
+            List<ProductOrder> freePromotionProducts = promotionService.checkFreePromotionProduct(orders);
+            for(ProductOrder productOrder: freePromotionProducts){
+                String productName = productOrder.getProductName();
+                int quantity = productOrder.getQuantity();
+
+                outputView.displayPromotionSuggestion(productName, quantity);
+                boolean decisionInput = inputView.continueShopping();
+
+                if(decisionInput){
+                    order.addPromotionalProductOrder(productName, quantity);
+                }
+            }
+
+            List<ProductOrder> withoutPromoStock = promotionService.checkWithoutPromotionStock(orders);
+            for(ProductOrder productOrder: withoutPromoStock){
+                String productName = productOrder.getProductName();
+                int quantity = productOrder.getQuantity();
+                outputView.displayPromotionLimitWarning(productName, quantity);
+                boolean decisionInput = inputView.continueShopping();
+
+                if(!decisionInput){
+                    order.reduceTotalProductOrder(productName, quantity);
+                }
+            }
+
+            if(inputView.applyMembershipDiscount()){
+                order.setMembershipApplied(true);
+            }
+
+            System.out.println(order.getTotalProductOrders());
+            System.out.println(order.getPromotionalProductOrders());
+
+            // todo : 구매 결과 및 영수증 출력
+
+            if(!inputView.continueShopping()){
+                break;
+            }
+        }
+    }
+
+    private List<ProductOrder> InputPurchase(){
+        while (true){
+            try{
+                List<ProductOrder> orders = inputView.getProductOrders();
+                inventoryService.checkProductStock(orders);
+                return orders;
+            }catch (IllegalArgumentException e){
+                outputView.displayError(e.getMessage());
+            }
+        }
+    }
+}

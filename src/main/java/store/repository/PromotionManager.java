a472f5f7819998ee3f2beb762b9f2a7d62023f9b
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-10 22:55:51 +0900

    modify(PromotionManager): 프로모션 Manager에 DataLoader 주입

--- src/main/java/store/repository/PromotionManager.java
+++ src/main/java/store/repository/PromotionManager.java
@@ -1,7 +1,9 @@
 package store.repository;
 
 import store.domain.Promotion;
+import store.infra.PromotionDataLoader;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,8 +11,19 @@ import java.util.Map;
 public class PromotionManager {
     private Map<String, Promotion> promotions = new HashMap<>();
 
-    public PromotionManager(List<Promotion> promotions) {
-        for (Promotion promotion : promotions) {
+    // PromotionDataLoader를 주입받아 초기화하는 생성자
+    public PromotionManager(PromotionDataLoader dataLoader) {
+        try {
+            List<Promotion> promotionsList = dataLoader.loadPromotions();
+            initializePromotions(promotionsList);
+        } catch (IOException e) {
+            throw new IllegalStateException("프로모션 데이터를 로드하는 중 오류 발생", e);
+        }
+    }
+
+    // 프로모션 데이터를 초기화하는 메서드
+    private void initializePromotions(List<Promotion> promotionsList) {
+        for (Promotion promotion : promotionsList) {
             this.promotions.put(promotion.getName(), promotion);
         }
     }

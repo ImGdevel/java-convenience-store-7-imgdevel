a5a824631931dc481da5fbaa20c53dd09f47587e
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-10 14:46:13 +0900

    test(Product): Product에 맞춰 Test 수정 및 추가

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,50 +1,55 @@
 package store.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+    private Product product;
 
-    private Product createProduct() {
-        return new Product("콜라", 1000, 10, "탄산2+1");
+    @BeforeEach
+    void setup() {
+        product = new Product("콜라", 1000);
+        product.setRegularStock(10);
+        product.setPromotion("탄산2+1");
+        product.setPromotionStock(5);
     }
 
     @Test
-    void 프로모션이_있을_경우_출력() {
-        Product product = createProduct();
-
-        assertEquals("콜라 1,000원 10개 탄산2+1", product.toString());
+    void 프로모션_가능한지_확인() {
+        assertTrue(product.isPromotionAvailable());
     }
 
     @Test
-    void 프로모션이_없을_경우_출력() {
-        Product product = new Product("사이다", 1000, 8, null);
-
-        assertEquals("사이다 1,000원 8개", product.toString());
+    void 프로모션재고가_없을때_프로모션_불가능() {
+        product.setPromotionStock(0);
+        assertFalse(product.isPromotionAvailable());
     }
 
     @Test
-    void 재고가_차감되는지_확인() {
-        Product product = createProduct();
-
-        product.reduceStock(3);
-        assertEquals(7, product.getStock());
+    void 정규재고_감소_확인() {
+        product.reduceStock(3, false);
+        assertEquals(7, product.getRegularStock());
     }
 
     @Test
-    void 재고가_0이_될_때_차감후_재고확인() {
-        Product product = createProduct();
-
-        product.reduceStock(10);
-        assertEquals(0, product.getStock());
-        assertEquals("콜라 1,000원 재고 없음 탄산2+1", product.toString());
+    void 프로모션재고_감소_확인() {
+        product.reduceStock(2, true);
+        assertEquals(3, product.getPromotionStock());
     }
 
     @Test
-    void 재고_초과_차감_예외_확인() {
-        Product product = createProduct();
+    void 정규재고_초과_차감시_예외_확인() {
+        assertThrows(IllegalArgumentException.class, () -> product.reduceStock(11, false));
+    }
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> product.reduceStock(11));
-        assertEquals("재고 수량을 초과하여 구매할 수 없습니다.", exception.getMessage());
+    @Test
+    void 프로모션재고_초과_차감시_예외_확인() {
+        assertThrows(IllegalArgumentException.class, () -> product.reduceStock(6, true));
+    }
+
+    @Test
+    void 프로모션_세부사항_설정_확인() {
+        assertEquals("탄산2+1", product.getPromotion());
     }
 }

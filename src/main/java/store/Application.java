42939890761787b76ae7e1ca53d228e7686b190b
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-11 13:15:17 +0900

    refactor(Application): 코드 축약 수정

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -5,6 +5,7 @@ import store.controller.ConvenienceStore;
 
 public class Application {
     public static void main(String[] args) {
-        new ConvenienceStore().start();
+        ConvenienceStore app = new ConvenienceStore();
+        app.start();
     }
 }

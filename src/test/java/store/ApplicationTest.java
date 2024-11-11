a754f66d4902f0fcf846d0ed3db849e320658392
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-11 13:12:41 +0900

    test(ApplicationTest): 예외 처리 테스트 추가

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -54,10 +54,26 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 재고_수량_초과_예외_테스트() {
         assertSimpleTest(() -> {
             runException("[컵라면-12]", "N", "N");
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 존재하지_않는_상품_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("[우아한테크코스-1]", "N", "N");
+            assertThat(output()).contains("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 올바르지_않은_형식_입력_테스트() {
+        assertSimpleTest(() -> {
+            runException("[콜라-10개]", "N", "N");
+            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         });
     }
 
